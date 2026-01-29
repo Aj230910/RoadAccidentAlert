c22776b2fbb93bb97681d65ac41319820a94838b
Ambrish Jeyan <ambrishjeyan023@gmail.com>
2026-01-29 22:32:57 +0530

    Initial commit - Road Accident Alert System

---
+++ test_email.py
@@ -0,0 +1,7 @@
+from email_alert import send_email_alert
+
+send_email_alert(
+    "🚨 TEST EMAIL WITH SNAPSHOT",
+    "If you see an attachment, snapshot email works perfectly.",
+    "snapshots/test.jpg"
+)
